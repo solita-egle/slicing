c9e9cc366e5d253695b5f336d18354354f74ad86
Egle Palk <egle.palk@solita.ee>
2021-04-19 15:33:55 +0300

    Added more validation

--- retail_calculator/retail_calculator.py
+++ retail_calculator/retail_calculator.py
@@ -34,37 +34,61 @@ def get_discount(order_value: float):
 def get_user_input():
     while True:
         try:
-            nr_of_items = input('Please enter the nr of items: ')
+            nr_of_items = int(input('Please enter the nr of items: '))
+           #user_input_nr_of_items_validation(nr_of_items)
         except ValueError:
-            print('Wrong input. Please enter the number again')
             continue
         else:
             break
     while True:
         try:
             price = input('Please enter the price of an item: ')
+            user_input_price_validation(price)
         except ValueError:
-            print('Wrong input. Please enter the number again')
             continue
         else:
             break
     while True:
         try:
             state_code = input('Please enter the state code: ')
+            user_input_state_code_validation(state_code)
         except ValueError:
-            print('Wrong input. Please enter the state code again')
             continue
         else:
             break
 
-def user_input_validation(nr_of_items,price,state_code):
-    if not isinstance(nr_of_items, int):
-        print("Value of number of items should be integer. Please enter the value again")
-        return False
-    if nr_of_items <= 0:
+
+def user_input_nr_of_items_validation(nr_of_items):
+    #try:
+        #nr_of_items = int(nr_of_items.strip())
+    #except ValueError as e:
+      #  raise ValueError
+    #if not isinstance(int(nr_of_items), int):
+       # print("Value of number of items should be integer. Please enter the value again")
+       # raise ValueError
+    if int(nr_of_items) <= 0:
         print("Value of number of items should be greater than 0. Please enter the value again")
-        return False
-    
+        raise ValueError
+
+
+def user_input_price_validation(price):
+    if not isinstance(float(price), float):
+        print("Value price should be integer. Please enter the value again")
+        raise ValueError
+    if float(price) <= 0:
+        print("Value of price should be greater than 0. Please enter the value again")
+        raise ValueError
+
+
+def user_input_state_code_validation(state_code):
+    if not isinstance(str(state_code), str):
+        print("Value of state code should be string. Please enter the value again")
+        raise ValueError
+    if str(state_code) not in TAX_RATES.keys():
+        print("Allowed values are UT, NV, TX, AL, CA")
+        raise ValueError
+
 
 if __name__ == '__main__':
-    user_input_validation('test', 51.15, 'UT')
+    # user_input_validation('test', 51.15, 'UT')
+    get_user_input()
